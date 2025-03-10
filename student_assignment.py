4ce0d30a5576aa2dbf2ec32d440fc973f1328436
jerry10_chang <jerry10_chang@asus.com>
2025-03-10 17:21:19 +0800

    test_hw1
    test_hw1

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,8 @@
 import datetime
 import chromadb
 import traceback
+import pandas as pd
+import time
 
 from chromadb.utils import embedding_functions
 
@@ -12,7 +14,45 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 dbpath = "./"
 
 def generate_hw01():
-    pass
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+
+    df = pd.read_csv("COA_OpenData.csv")
+    for _, row in df.iterrows():
+        try:
+            metadata = {
+                "file_name": "COA_OpenData.csv",
+                "name": row["Name"],
+                "type": row["Type"],
+                "address": row["Address"],
+                "tel": row["Tel"],
+                "city": row["City"],
+                "town": row["Town"],
+                "date": row["CreateDate"],
+            }
+
+            id = row.get("ID", "")
+            document = row.get("HostWords", "")
+            collection.update(ids=[id], documents=[document], metadatas=[metadata])
+
+        except Exception as e:
+            print(f"Error inserting row: {row}")
+            traceback.print_exc()
+
+    return collection
+
+
     
 def generate_hw02(question, city, store_type, start_date, end_date):
     pass
@@ -36,3 +76,5 @@ def demo(question):
     )
     
     return collection
+
+print(generate_hw01())
