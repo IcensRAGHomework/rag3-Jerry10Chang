cb9b6d57d0fbdd73491bb3dfa1d3a75c0a357ff8
jerry10_chang <jerry10_chang@asus.com>
2025-03-10 17:44:10 +0800

    test
    test

--- student_assignment.py
+++ student_assignment.py
@@ -16,23 +16,23 @@ dbpath = "./"
 def generate_hw01():
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-        api_key = gpt_emb_config['api_key'],
-        api_base = gpt_emb_config['api_base'],
-        api_type = gpt_emb_config['openai_type'],
-        api_version = gpt_emb_config['api_version'],
-        deployment_id = gpt_emb_config['deployment_name']
+        api_key=gpt_emb_config['api_key'],
+        api_base=gpt_emb_config['api_base'],
+        api_type=gpt_emb_config['openai_type'],
+        api_version=gpt_emb_config['api_version'],
+        deployment_id=gpt_emb_config['deployment_name']
     )
+    
     collection = chroma_client.get_or_create_collection(
         name="TRAVEL",
         metadata={"hnsw:space": "cosine"},
         embedding_function=openai_ef
     )
-
-    df = pd.read_csv("COA_OpenData.csv")
-    for _, row in df.iterrows():
-        try:
+    if collection.count() == 0:
+        df = pd.read_csv(csv_file)
+        for _, row in df.iterrows():
             metadata = {
-                "file_name": "COA_OpenData.csv",
+                "file_name": csv_file,
                 "name": row["Name"],
                 "type": row["Type"],
                 "address": row["Address"],
@@ -41,14 +41,10 @@ def generate_hw01():
                 "town": row["Town"],
                 "date": int(datetime.datetime.strptime(row["CreateDate"], "%Y-%m-%d").timestamp())
             }
-
-            id = str(row["ID"])
+            
             document = row.get("HostWords", "")
-            collection.add(ids=[id], documents=[document], metadatas=[metadata])
-
-        except Exception as e:
-            print(f"Error inserting row: {row}")
-            traceback.print_exc()
+            document_id = str(row["ID"])  
+            collection.add(ids=[document_id], documents=[document], metadatas=[metadata])
 
     return collection
 
